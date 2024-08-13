e5ad5efd86cb129fcc7862762cd1dc531f81d03d
ciaqui <ciara@u1trav101.net>
2024-08-13 02:17:42 +0200

    added stylelint

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     plugins: [
       require("autoprefixer"),
+      require("stylelint"),
       ...(process.env.JEKYLL_ENV == "production"
         ? [require("cssnano")({ preset: "default" })]
         : [])
